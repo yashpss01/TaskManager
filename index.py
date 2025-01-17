9441dfd4fa85ac28d19ee99d05d587ef5438a4b4
yashpss01 <yashpratap0143@gmail.com>
2025-01-17 18:27:01 +0530

    Create index.py and added main menu

---
+++ index.py
@@ -0,0 +1,10 @@
+# function to call the main menu.
+def menu():
+    print("-------Task Manager-------",end="\n\n")
+    print("1. Add a new task.")
+    print("2. View tasks.")
+    print("3. Marks the task as completed.")
+    print("4. Delete a task.")
+    print("5. Exit.",end="\n\n")
+    inp=input("Choose your preferred option :: ")
+option=menu()
